5657b7769c60479d5849bd1f1dc87f18ff498678
Nicholas Ward <npward@berkeley.edu>
2023-07-25 15:37:57 -0700

    cleanup

--- blake3/src/lib.rs
+++ blake3/src/lib.rs
@@ -5,7 +5,8 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
-use blake3::{MAX_SIMD_DEGREE_OR_2, hash_many_wrapper};
+
+use blake3::{hash_many_wrapper, MAX_SIMD_DEGREE_OR_2};
 use itertools::Itertools;
 use p3_symmetric::hasher::CryptographicHasher;
 
@@ -22,10 +23,20 @@ impl CryptographicHasher<u8, [[u8; 32]; VSIZE]> for Blake3Hash {
         let input_vec = input.into_iter().collect::<Vec<_>>();
         let chunk_size = input_vec.len() / VSIZE;
         let input_chunks = input_vec.chunks(chunk_size);
-        let input_chunks_array: [&[u8]; VSIZE] = input_chunks.map(|chunk| chunk.try_into().unwrap()).collect_vec().try_into().unwrap();
+        let input_chunks_array: [&[u8]; VSIZE] = input_chunks
+            .map(|chunk| chunk.try_into().unwrap())
+            .collect_vec()
+            .try_into()
+            .unwrap();
 
         let output = hash_many_wrapper(&input_chunks_array);
-        output.intern.chunks(chunk_size).map(|chunk| chunk.try_into().unwrap()).collect_vec().try_into().unwrap()
+        output
+            .intern
+            .chunks(chunk_size)
+            .map(|chunk| chunk.try_into().unwrap())
+            .collect_vec()
+            .try_into()
+            .unwrap()
     }
 
     fn hash_iter_slices<'a, I>(&self, _input: I) -> [[u8; 32]; VSIZE]
