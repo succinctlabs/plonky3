363ae9f0be7de0f4dcfb4c18c36cfae318a401ea
Nicholas Ward <npward@berkeley.edu>
2023-07-25 15:37:25 -0700

    implementing CryptographicHasher instead

--- blake3/src/lib.rs
+++ blake3/src/lib.rs
@@ -6,25 +6,32 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use blake3::{MAX_SIMD_DEGREE_OR_2, hash_many_wrapper};
-use p3_symmetric::compression::PseudoCompressionFunction;
+use itertools::Itertools;
+use p3_symmetric::hasher::CryptographicHasher;
 
 /// The BLAKE3 permutation.
-pub struct Blake3Compression;
+pub struct Blake3Hash;
 
-const COMPRESS_OUT_LEN: usize = 32 * MAX_SIMD_DEGREE_OR_2;
+const VSIZE: usize = MAX_SIMD_DEGREE_OR_2;
 
-impl PseudoCompressionFunction<[u8; COMPRESS_OUT_LEN], 32> for Blake3Compression {
-    fn compress(&self, input: [[u8; COMPRESS_OUT_LEN]; 32]) -> [u8; COMPRESS_OUT_LEN] {
-        let input_flattened = input.into_iter().flatten().collect::<Vec<_>>();
-        let mut input_arrays_vec: Vec<[u8; 1024]> = Vec::new();
-        for idx in 0..MAX_SIMD_DEGREE_OR_2 {
-            input_arrays_vec.push(input_flattened[idx * 1024..(idx + 1) * 1024].try_into().unwrap());
-        }
-        let input_arrays_vec_cloned = input_arrays_vec.clone();
-        let input_slices_vec: Vec<&[u8]> = input_arrays_vec_cloned.iter().map(|a| a.as_ref()).collect();
-        let input_resized: [&[u8]; MAX_SIMD_DEGREE_OR_2] = input_slices_vec.try_into().unwrap();
+impl CryptographicHasher<u8, [[u8; 32]; VSIZE]> for Blake3Hash {
+    fn hash_iter<I>(&self, input: I) -> [[u8; 32]; VSIZE]
+    where
+        I: IntoIterator<Item = u8>,
+    {
+        let input_vec = input.into_iter().collect::<Vec<_>>();
+        let chunk_size = input_vec.len() / VSIZE;
+        let input_chunks = input_vec.chunks(chunk_size);
+        let input_chunks_array: [&[u8]; VSIZE] = input_chunks.map(|chunk| chunk.try_into().unwrap()).collect_vec().try_into().unwrap();
 
-        let output = hash_many_wrapper(&input_resized);
-        output.intern
+        let output = hash_many_wrapper(&input_chunks_array);
+        output.intern.chunks(chunk_size).map(|chunk| chunk.try_into().unwrap()).collect_vec().try_into().unwrap()
+    }
+
+    fn hash_iter_slices<'a, I>(&self, input: I) -> [[u8; 32]; VSIZE]
+    where
+        I: IntoIterator<Item = &'a [u8]>,
+    {
+        todo!()
     }
 }
