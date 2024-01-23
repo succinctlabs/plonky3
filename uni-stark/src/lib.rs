70cc7d6af035c0f7dc4e3c9726e645d56c994ba0
Tamir Hemo <tamir@succinct.xyz>
2024-01-23 12:01:21 -0800

    pub symbolic variable

--- uni-stark/src/lib.rs
+++ uni-stark/src/lib.rs
@@ -25,5 +25,6 @@ pub use proof::*;
 pub use prover::*;
 pub use symbolic_builder::*;
 pub use symbolic_expression::*;
+pub use symbolic_variable::*;
 pub use verifier::*;
 pub use zerofier_coset::*;
