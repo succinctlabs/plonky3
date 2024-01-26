11aeac92f5ae87849ce6c302736c1e8ee8d6e406
Kevin Jue <kjue235@gmail.com>
2024-01-26 06:00:05 -0800

    making more things public

--- keccak-air/src/lib.rs
+++ keccak-air/src/lib.rs
@@ -14,11 +14,10 @@ mod round_flags;
 pub use air::*;
 pub use columns::*;
 pub use generation::*;
-
 pub use constants::RC;
 
 pub const NUM_ROUNDS: usize = 24;
 const BITS_PER_LIMB: usize = 16;
-const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
+pub const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
 const RATE_BITS: usize = 1088;
 const RATE_LIMBS: usize = RATE_BITS / BITS_PER_LIMB;
