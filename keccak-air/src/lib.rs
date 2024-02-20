cbb720b0f6b1d7baff823bb43f9a0b4dd531f525
Tamir Hemo <tamir@succinct.xyz>
2024-02-20 12:05:50 -0800

    make pub

--- keccak-air/src/lib.rs
+++ keccak-air/src/lib.rs
@@ -17,6 +17,6 @@ pub use generation::*;
 
 const NUM_ROUNDS: usize = 24;
 const BITS_PER_LIMB: usize = 16;
-const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
+pub const U64_LIMBS: usize = 64 / BITS_PER_LIMB;
 const RATE_BITS: usize = 1088;
 const RATE_LIMBS: usize = RATE_BITS / BITS_PER_LIMB;
