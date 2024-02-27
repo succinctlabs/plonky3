99a987f5b1bd241d6a4cd18b3e6f9d00493fde57
Kevin Jue <kjue235@gmail.com>
2024-02-27 13:02:48 -0800

    some fixes

--- symmetric/src/serializing_hasher.rs
+++ symmetric/src/serializing_hasher.rs
@@ -1,8 +1,4 @@
-use core::borrow::Borrow;
-use core::marker::PhantomData;
-
-use p3_field::{PrimeField32, PrimeField64};
-use serde::{Deserialize, Serialize};
+use p3_field::{PackedField, PackedValue, PrimeField32, PrimeField64};
 
 use crate::CryptographicHasher;
 
@@ -10,16 +6,6 @@ use crate::CryptographicHasher;
 #[derive(Copy, Clone)]
 pub struct SerializingHasher32<Inner> {
     inner: Inner,
-}
-
-/// A wrapper around an array digest, with a phantom type parameter to ensure that the digest is
-/// associated with a particular field.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(bound(serialize = "[W; DIGEST_ELEMS]: Serialize"))]
-#[serde(bound(deserialize = "[W; DIGEST_ELEMS]: Deserialize<'de>"))]
-pub struct Hash<F, W, const DIGEST_ELEMS: usize> {
-    value: [W; DIGEST_ELEMS],
-    _marker: PhantomData<F>,
 }
 
 /// Maps input field elements to their 8-byte little-endian encodings, outputs `[u8; 32]`.
@@ -57,21 +43,22 @@ where
     }
 }
 
-impl<F, Inner> CryptographicHasher<F, [u32; 8]> for SerializingHasher32<Inner>
+impl<P, PW, Inner> CryptographicHasher<P, [PW; 8]> for SerializingHasher32<Inner>
 where
-    F: PrimeField32,
-    Inner: CryptographicHasher<u32, [u32; 8]>,
+    P: PackedField,
+    P::F: PrimeField32,
+    PW: PackedValue<Value = u32>,
+    Inner: CryptographicHasher<PW, [PW; 8]>,
 {
-    fn hash_iter<I>(&self, input: I) -> [u32; 8]
+    fn hash_iter<I>(&self, input: I) -> [PW; 8]
     where
-        I: IntoIterator<Item = F>,
+        I: IntoIterator<Item = P>,
     {
-        let ret = self.inner.hash_iter(
+        self.inner.hash_iter(
             input
                 .into_iter()
-                .map(|x| x.as_canonical_u32())
-        );
-        ret
+                .map(|x| PW::from_fn(|i| x.as_slice()[i].as_canonical_u32())),
+        )
     }
 }
 
@@ -92,46 +79,21 @@ where
     }
 }
 
-impl<F, W, const DIGEST_ELEMS: usize> From<[W; DIGEST_ELEMS]> for Hash<F, W, DIGEST_ELEMS> {
-    fn from(value: [W; DIGEST_ELEMS]) -> Self {
-        Self {
-            value,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<F, W, const DIGEST_ELEMS: usize> From<Hash<F, W, DIGEST_ELEMS>> for [W; DIGEST_ELEMS] {
-    fn from(value: Hash<F, W, DIGEST_ELEMS>) -> [W; DIGEST_ELEMS] {
-        value.value
-    }
-}
-
-impl<F, W: PartialEq, const DIGEST_ELEMS: usize> PartialEq<[W; DIGEST_ELEMS]>
-    for Hash<F, W, DIGEST_ELEMS>
+impl<P, PW, Inner> CryptographicHasher<P, [PW; 4]> for SerializingHasher64<Inner>
+where
+    P: PackedField,
+    P::F: PrimeField64,
+    PW: PackedValue<Value = u64>,
+    Inner: CryptographicHasher<PW, [PW; 4]>,
 {
-    fn eq(&self, other: &[W; DIGEST_ELEMS]) -> bool {
-        self.value == *other
-    }
-}
-
-impl<F, W, const DIGEST_ELEMS: usize> IntoIterator for Hash<F, W, DIGEST_ELEMS> {
-    type Item = W;
-    type IntoIter = core::array::IntoIter<W, DIGEST_ELEMS>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.value.into_iter()
-    }
-}
-
-impl<F, W, const DIGEST_ELEMS: usize> Borrow<[W; DIGEST_ELEMS]> for Hash<F, W, DIGEST_ELEMS> {
-    fn borrow(&self) -> &[W; DIGEST_ELEMS] {
-        &self.value
-    }
-}
-
-impl<F, W, const DIGEST_ELEMS: usize> AsRef<[W; DIGEST_ELEMS]> for Hash<F, W, DIGEST_ELEMS> {
-    fn as_ref(&self) -> &[W; DIGEST_ELEMS] {
-        &self.value
+    fn hash_iter<I>(&self, input: I) -> [PW; 4]
+    where
+        I: IntoIterator<Item = P>,
+    {
+        self.inner.hash_iter(
+            input
+                .into_iter()
+                .map(|x| PW::from_fn(|i| x.as_slice()[i].as_canonical_u64())),
+        )
     }
 }
