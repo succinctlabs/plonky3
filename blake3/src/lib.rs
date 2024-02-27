2bac0354c66baac2f33c6f1116ade98009085a4d
Kevin Jue <kjue235@gmail.com>
2024-02-27 12:45:27 -0800

    u32 related hash functions

--- blake3/src/lib.rs
+++ blake3/src/lib.rs
@@ -35,3 +35,35 @@ impl CryptographicHasher<u8, [u8; 32]> for Blake3 {
         hasher.finalize().into()
     }
 }
+
+impl CryptographicHasher<u32, [u32; 8]> for Blake3 {
+    fn hash_iter<I>(&self, input: I) -> [u32; 8]
+    where
+        I: IntoIterator<Item = u32>,
+    {
+        let mut input = input.into_iter().collect::<Vec<_>>();
+        if input.len() <= blake3_zkvm::BLOCK_LEN {
+            let size = input.len();
+            input.resize(blake3_zkvm::BLOCK_LEN, 0u32);
+            blake3_zkvm::hash_single_block(input.as_slice().try_into().unwrap(), size)
+        } else {
+            let ret = self.hash_iter_slices([input.as_slice()]);
+            ret
+        }
+    }
+
+    fn hash_iter_slices<'a, I>(&self, input: I) -> [u32; 8]
+    where
+        I: IntoIterator<Item = &'a [u32]>,
+    {
+        let mut zkvm_hasher = blake3_zkvm::Hasher::new();
+
+        for chunk in input.into_iter() {
+            zkvm_hasher.update(chunk);
+        }
+        let mut out: [u32; 8] = [0u32; 8];
+        zkvm_hasher.finalize(&mut out);
+
+        out
+    }
+}
